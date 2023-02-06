414faad3ea5eed40dd576e50ff1d0fc043ff2240
Sergei Egorov <null@egorbos.codes>
2023-02-06 21:48:07 +1000

    Добавлено решение задачи № 19

--- Lesson 3/Program.cs
+++ Lesson 3/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача № 19
+bool IsNumberPalindrome(int number)
+{
+    var tail = number % 100;
+    return number / 1000 == tail / 10 + (tail % 10) * 10;
+}
+
+Console.Clear();
+Console.WriteLine("Задача № 19");
+Console.Write("Введите пятизначное число: ");
+var taskNineteenNum = Convert.ToInt32(Console.ReadLine());
+
+while (taskNineteenNum < 10000 || taskNineteenNum > 99999)
+{
+    Console.Write("Число должно быть пятизначным! Введите число: ");
+    taskNineteenNum = Convert.ToInt32(Console.ReadLine());
+}
+
+Console.WriteLine(IsNumberPalindrome(taskNineteenNum) ? "Да" : "Нет");
