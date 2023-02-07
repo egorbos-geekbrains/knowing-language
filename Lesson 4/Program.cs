416590d315ef6d6759c5f9e7ea1c139db0c9502a
Sergei Egorov <null@egorbos.codes>
2023-02-07 11:42:02 +1000

    Добавлено решение задачи № 29

--- Lesson 4/Program.cs
+++ Lesson 4/Program.cs
@@ -23,6 +23,25 @@ int GetDigitsSumm(int number)
     return result;
 }
 
+// Задача № 29
+int GetPupilPosition(int[] pupils, int pupilHeight)
+{
+    if (pupils[0] < pupilHeight)
+    {
+        return 1;
+    }
+    var prevHeight = pupils[0];
+    for (var i = 1; i < pupils.Length; i++)
+    {
+        if (pupilHeight <= prevHeight && pupilHeight > pupils[i])
+        {
+            return i + 1;
+        }
+        prevHeight = pupils[i];
+    }
+    return pupils.Length + 1;
+}
+
 //
 Console.Clear();
 Console.WriteLine("Задача № 25");
@@ -40,7 +59,7 @@ while (taskTwentyFiveNumB == 0)
 Console.WriteLine(Pow(taskTwentyFiveNumA, taskTwentyFiveNumB));
 
 //
-Console.WriteLine("Задача № 27");
+Console.WriteLine("\nЗадача № 27");
 Console.Write("Введите число: ");
 var taskTwentySevenNum = Convert.ToInt32(Console.ReadLine());
 
@@ -51,3 +70,44 @@ while (taskTwentySevenNum < 10)
 }
 
 Console.WriteLine(GetDigitsSumm(taskTwentySevenNum));
+
+//
+Console.WriteLine("\nЗадача № 29");
+Console.Write("Введите количество учеников: ");
+var pupilsCount = Convert.ToInt32(Console.ReadLine());
+
+while (pupilsCount < 1 || pupilsCount > 100)
+{
+    Console.Write("Количество должно быть в диапазоне [1; 100]! Введите количество учеников: ");
+    pupilsCount = Convert.ToInt32(Console.ReadLine());
+}
+
+var pupils = new int[pupilsCount];
+for (var i = 0; i < pupilsCount; i++)
+{
+    Console.Write($"Введите рост ученика № {i + 1}: ");
+    pupils[i] = Convert.ToInt32(Console.ReadLine());
+    while (pupils[i] < 1 || (i != 0 && pupils[i] > pupils[i - 1]))
+    {
+        if (pupils[i] < 1)
+        {
+            Console.Write("Рост должен быть в диапазоне [1; 200]! Введите рост: ");
+        }
+        else
+        {
+            Console.Write("Рост должен быть меньше, чем у предыдущего ученика! Введите рост: ");
+        }
+        pupils[i] = Convert.ToInt32(Console.ReadLine());
+    }
+}
+
+Console.Write("Введите рост Пети: ");
+var petyaHeight = Convert.ToInt32(Console.ReadLine());
+
+while (petyaHeight < 1)
+{
+    Console.Write("Рост должен быть в диапазоне [1; 200]! Введите рост: ");
+    petyaHeight = Convert.ToInt32(Console.ReadLine());
+}
+
+Console.WriteLine(GetPupilPosition(pupils, petyaHeight));
