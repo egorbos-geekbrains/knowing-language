a0423a5f849bb9527be9b28be738f66110acccae
Sergei Egorov <null@egorbos.codes>
2023-02-18 19:56:08 +1000

    Добавлено решение задачи № 43

--- Lesson 6/Program.cs
+++ Lesson 6/Program.cs
@@ -17,3 +17,29 @@ while (parsed)
     }
 }
 Console.WriteLine($"Количество чисел больше нуля: {positiveCount}");
+
+//
+Console.WriteLine("\nЗадача № 43");
+Console.Write("k1=");
+var kOne = Convert.ToDouble(Console.ReadLine());
+Console.Write("b1=");
+var bOne = Convert.ToDouble(Console.ReadLine());
+Console.Write("k2=");
+var kTwo = Convert.ToDouble(Console.ReadLine());
+Console.Write("b2=");
+var bTwo = Convert.ToDouble(Console.ReadLine());
+
+if (kOne != kTwo)
+{
+    var x = (bTwo - bOne) / (kOne - kTwo);
+    var y = (kOne * (bTwo - bOne)) / (kOne - kTwo) + bOne;
+    Console.WriteLine($"Точка пересечения имеет координаты ({x}; {y})");
+}
+else if (bOne == bTwo)
+{
+    Console.WriteLine("Прямые совпадают");
+}
+else
+{
+    Console.WriteLine("Прямые параллельны");
+}
