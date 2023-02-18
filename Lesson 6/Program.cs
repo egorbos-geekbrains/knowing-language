59efec960712a5399173a3702956750aaac91e13
Sergei Egorov <null@egorbos.codes>
2023-02-18 13:32:50 +1000

    Добавлено решение задачи № 41

--- Lesson 6/Program.cs
+++ Lesson 6/Program.cs
@@ -1,1 +1,19 @@
-﻿
+﻿//
+Console.Clear();
+var random = new Random();
+
+//
+Console.Write("\nЗадача № 41");
+var parsed = true;
+var parseResult = 0;
+var positiveCount = 0;
+while (parsed)
+{
+    Console.Write($"\nВведите число (или любой символ для окончания ввода): ");
+    parsed = int.TryParse(Console.ReadLine(), out parseResult);
+    if (parseResult > 0)
+    {
+        positiveCount++;
+    }
+}
+Console.WriteLine($"Количество чисел больше нуля: {positiveCount}");
