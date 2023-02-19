a501a43e06ed175c8d88b14ec7d2f22f99e01a5e
Sergei Egorov <null@egorbos.codes>
2023-02-19 12:29:25 +1000

    Добавлено решение задачи 'Рекурсия'

--- Lesson 6/Program.cs
+++ Lesson 6/Program.cs
@@ -1,6 +1,40 @@
-﻿//
+﻿// Рекурсия
+int GetFactorial(int number)
+{
+    return number == 1 ? 1 : number * GetFactorial(number - 1);
+}
+
+string[] GetPermutes(string text)
+{
+    var currentIndex = 0;
+    var permutes = new string[GetFactorial(text.Length)];    
+    void permute(char[] chars, int length, string currentPermute = "")
+    {
+        foreach (var character in chars.Reverse())
+        {
+            var permuteText = currentPermute;
+            if(permuteText.Length == chars.Length)
+            {
+                permutes[currentIndex] = permuteText;
+                currentIndex++;
+            }
+            if (length == 0)
+            {
+                return;
+            }
+            if (!currentPermute.Contains(character))
+            {
+                permuteText += character;
+            }
+            permute(chars, length - 1, permuteText);
+        }
+    };
+    permute(text.ToCharArray(), text.Length);
+    return permutes;
+}
+
+//
 Console.Clear();
-var random = new Random();
 
 //
 Console.Write("\nЗадача № 41");
@@ -9,7 +43,7 @@ var parseResult = 0;
 var positiveCount = 0;
 while (parsed)
 {
-    Console.Write($"\nВведите число (или любой символ для окончания ввода): ");
+    Console.Write($"\nВведите число (или введите любой символ для окончания ввода): ");
     parsed = int.TryParse(Console.ReadLine(), out parseResult);
     if (parseResult > 0)
     {
@@ -42,4 +76,20 @@ else if (bOne == bTwo)
 else
 {
     Console.WriteLine("Прямые параллельны");
+}
+
+//
+Console.WriteLine("\nРекурсия");
+Console.Write("Введите исходную строку: ");
+var text = Console.ReadLine()?.ToUpperInvariant();
+while (text is null || text.Trim().Length == 0 || text.Trim().Length > 8)
+{
+    Console.Write("Длина строки должна быть в диапазоне [1; 8]! Введите исходную строку: ");
+    text = Console.ReadLine()?.ToUpperInvariant();
+}
+var permutes = GetPermutes(text);
+Console.WriteLine("Возможные перестановки:");
+foreach (var item in permutes)
+{
+    Console.WriteLine(item);
 }
