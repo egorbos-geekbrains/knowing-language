7c273c56b4a31cdd35075845c976e6ae9f7ecc1f
Sergei Egorov <null@egorbos.codes>
2023-02-24 19:45:50 +1000

    Добавлено решение задачи № 47

--- Lesson 7/Program.cs
+++ Lesson 7/Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Вспомогательные функции
+void InputMatrix<T>(T[,] matrix, Func<T> generator)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = generator();
+        }
+    }
+}
+
+void PrintMatrix<T>(T[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} \t");
+        }
+        Console.WriteLine();
+    }
+}
+
+// Задача № 47
+
+
+//
+Console.Clear();
+var random = new Random();
+
+//
+Console.WriteLine("Задача № 47");
+const double MinValue = -10;
+const double MaxValue =  10;
+var matrixTaskFortySeven = new double[3, 4];
+InputMatrix(
+    matrixTaskFortySeven,
+    () => Math.Round(random.NextDouble() * (MaxValue - MinValue) + MinValue, 2)
+);
+PrintMatrix(matrixTaskFortySeven);
