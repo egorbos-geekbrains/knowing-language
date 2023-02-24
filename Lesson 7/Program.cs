33145a4fb9ceb1a21b033ba5f7ea1c2c71c2f367
Sergei Egorov <null@egorbos.codes>
2023-02-24 20:08:03 +1000

    Добавлено решение задачи № 52

--- Lesson 7/Program.cs
+++ Lesson 7/Program.cs
@@ -22,8 +22,21 @@ void PrintMatrix<T>(T[,] matrix)
     }
 }
 
-// Задача № 47
-
+// Задача № 52
+double[] CalculateColumnsAverage(int[,] matrix)
+{
+    var averages = new double[matrix.GetLength(1)];
+    for (int i = 0; i < matrix.GetLength(1); i++)
+    {
+        var average = .0;
+        for (int j = 0; j < matrix.GetLength(0); j++)
+        {
+            average += matrix[j, i];
+        }
+        averages[i] = Math.Round(average / matrix.GetLength(0), 1);
+    }
+    return averages;
+}
 
 //
 Console.Clear();
@@ -60,3 +73,11 @@ else
 {
     Console.WriteLine($"На данной позиции находится элемент {matrixTaskFifty[coordinates[0] - 1, coordinates[1] - 1]}");
 }
+
+//
+Console.WriteLine("\nЗадача № 52");
+var matrixTaskFiftyTwo = new int[3, 4];
+InputMatrix(matrixTaskFiftyTwo, () => random.Next(1, 10));
+PrintMatrix(matrixTaskFiftyTwo);
+var averageValues = CalculateColumnsAverage(matrixTaskFiftyTwo);
+Console.Write($"Среднее арифметическое каждого столбца: {string.Join("; ", averageValues)}");
