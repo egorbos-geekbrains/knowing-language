0277a2c451f61eb45b95a2b6d8c744f7ed36987a
Sergei Egorov <null@egorbos.codes>
2023-02-24 19:46:17 +1000

    Добавлено решение задачи № 50

--- Lesson 7/Program.cs
+++ Lesson 7/Program.cs
@@ -39,3 +39,24 @@ InputMatrix(
     () => Math.Round(random.NextDouble() * (MaxValue - MinValue) + MinValue, 2)
 );
 PrintMatrix(matrixTaskFortySeven);
+
+//
+Console.WriteLine("\nЗадача № 50");
+var matrixTaskFifty = new int[3, 4];
+InputMatrix(matrixTaskFifty, () => random.Next(1, 10));
+PrintMatrix(matrixTaskFifty);
+Console.Write("Введите позицию элемента: ");
+var coordinates = Console.ReadLine()?.Split(" ").Select(x => int.Parse(x)).ToArray();
+while (coordinates is null || coordinates.Length < 2 || coordinates.Any(e => e < 1))
+{
+    Console.Write("Введите позицию элемента: ");
+    coordinates = Console.ReadLine()?.Split(" ").Select(x => int.Parse(x)).ToArray();
+}
+if (matrixTaskFifty.GetLength(0) < coordinates[0] || matrixTaskFifty.GetLength(1) < coordinates[1])
+{
+    Console.WriteLine("Такой позиции в массиве нет");
+} 
+else
+{
+    Console.WriteLine($"На данной позиции находится элемент {matrixTaskFifty[coordinates[0] - 1, coordinates[1] - 1]}");
+}
