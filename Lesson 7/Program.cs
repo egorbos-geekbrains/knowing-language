945438f6f9ab1c85720157c228c4190adc28b1cd
Sergei Egorov <null@egorbos.codes>
2023-02-25 14:05:18 +1000

    Добавлено решение задачи 'Транспонирование'

--- Lesson 7/Program.cs
+++ Lesson 7/Program.cs
@@ -38,6 +38,23 @@ double[] CalculateColumnsAverage(int[,] matrix)
     return averages;
 }
 
+// Транспонирование
+void TransposeMatrix<T>(T[,] matrix)
+{
+    T tempValue;
+    var rows = matrix.GetLength(0);
+    var columns = matrix.GetLength(1);
+    for (var i = 0; i < rows / 2; i++)
+    {
+        for (var j = 0; j < columns; j++)
+        {
+            tempValue = matrix[i, j];
+            matrix[i, j] = matrix[rows - (i + 1), j];
+            matrix[rows - (i + 1), j] = tempValue;
+        }
+    }
+}
+
 //
 Console.Clear();
 var random = new Random();
@@ -81,3 +98,20 @@ InputMatrix(matrixTaskFiftyTwo, () => random.Next(1, 10));
 PrintMatrix(matrixTaskFiftyTwo);
 var averageValues = CalculateColumnsAverage(matrixTaskFiftyTwo);
 Console.Write($"Среднее арифметическое каждого столбца: {string.Join("; ", averageValues)}");
+
+//
+Console.WriteLine("\nТранспонирование");
+Console.Write("Введите размер матрицы: ");
+var transpositionMatrixSize = Console.ReadLine()?.Split(" ").Select(x => int.Parse(x)).ToArray();
+while (transpositionMatrixSize is null || transpositionMatrixSize.Length < 2 || transpositionMatrixSize.Any(e => e < 1))
+{
+    Console.Write("Введите размер матрицы: ");
+    transpositionMatrixSize = Console.ReadLine()?.Split(" ").Select(x => int.Parse(x)).ToArray();
+}
+var transpositionMatrix = new int[transpositionMatrixSize[0], transpositionMatrixSize[1]];
+InputMatrix(transpositionMatrix, () => random.Next(1, 10));
+Console.WriteLine("Исходная матрица:");
+PrintMatrix(transpositionMatrix);
+TransposeMatrix(transpositionMatrix);
+Console.WriteLine("Транспонированная матрица:");
+PrintMatrix(transpositionMatrix);
