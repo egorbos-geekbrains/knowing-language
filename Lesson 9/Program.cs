498fe22613c9937b4e36a6f3fd0a5bbe501275bd
Sergei Egorov <null@egorbos.codes>
2023-02-26 12:16:11 +1000

    Добавлено решение задачи № 66

--- Lesson 9/Program.cs
+++ Lesson 9/Program.cs
@@ -1,1 +1,28 @@
-﻿
+﻿// Вспомогательные функции
+int[]? GetValues()
+{
+    Console.Write($"\nВведите значения m, n разделяя пробелом: ");
+    var values = Console.ReadLine()?.Split(" ").Select(e => int.Parse(e)).ToArray();
+    return values;
+}
+
+// Задача № 66
+int GetNumbersRangeSum(int start, int end)
+{
+    if(start == end) return end;
+    return start + GetNumbersRangeSum(start + 1, end);
+}
+
+//
+Console.Clear();
+int[]? values;
+
+//
+Console.Write("\nЗадача № 66");
+values = GetValues();
+while (values is null || values.Length < 2 || values.Any(e => e < 1))
+{
+    values = GetValues();
+}
+var rangeSumm = GetNumbersRangeSum(values[0], values[1]);
+Console.WriteLine($"Сумма натуральных элементов в диапазоне от {values[0]} до {values[1]} равна {rangeSumm}");
